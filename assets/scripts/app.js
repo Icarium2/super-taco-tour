722675d61b49e5d0e5ccfc12a3a9188755c2228f
Icarium2 <70521736+Icarium2@users.noreply.github.com>
2021-03-18 13:11:11 +0100

    some fixes and logic for printing query params

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,17 @@
-console.log("fookin ello world m8");
+const getQuery = () => {
+  let queryParam = window.location.search;
+  queryParam = queryParam.replace("?", "");
+  queryParam = queryParam.split("&");
+
+  let queryObject = {};
+  queryParam.forEach((queryParam) => {
+    const splits = queryParam.split("=");
+    queryObject[splits[0]] = splits[1];
+  });
+  return queryObject;
+};
+
+const queryName = getQuery().name;
+const queryEmail = getQuery().email;
+
+console.log(queryName, queryEmail);
