f7e7e1555cce215887f663f256d659df94ee3116
Icarium2 <70521736+Icarium2@users.noreply.github.com>
2021-03-18 20:22:24 +0100

    touch-ups

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,7 +6,7 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
 if (urlParams.has("name", "email")) {
-  welcome.textContent = "välkomnar " + urlParams.get("name") + " till...";
+  welcome.textContent = "välkomnar  " + urlParams.get("name") + "  till...";
   formFill[0].value = urlParams.get("name");
   formFill[1].value = urlParams.get("email");
 }
